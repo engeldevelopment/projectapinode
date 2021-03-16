7025e7dcea90902d722d92dd6e85c32cf1013a99
engel <engeljavierpinto@gmail.com>
2021-03-16 19:47:45 -0400

    Agregué algunas dependencias y creé código de prueba.

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,14 +1,34 @@
-import request from 'supertest'
+import request  from 'supertest'
 
 import app from '../src/app'
 
-describe('App Server', () => {
+describe('Project API', () => {
+   let server = null
+   beforeAll(() => {
+      server = request(app)
+   })
    describe('GET /', () => {
-      it('deberia responder el message', (done) => {
-        const response = request(app).get('/')
-        const data = {"message": "Hi, i am typescript"}
+      it('should response the welcome message', (done) => {
+        const data = {"message": "Bienvenido a la api de projects!"}
+        
+        const response = server.get('/')
+        
         response.expect(200, data)
             .end(done)
-      }); 
-   }); 
+      });
+   });
+
+   describe('GET /api/v1/projects', () => {
+     it('should show a project list', (done) => {
+      const data = [{
+         name: "My new Projects",
+         cost: 100
+      }] 
+
+      const response = server.get('/api/v1/projects') 
+
+      response.expect(200, data)
+        .end(done)
+     }); 
+   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
+import router from './projects/routes' 
+
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 
+app.use('/api/v1', router)
+
+
 app.get('/', (req, resp) => {
-    resp.send({"message": "Hi, i am typescript"})
+    resp.send({"message": "Bienvenido a la api de projects!"})
 })
+
+
 
 
 export default app
